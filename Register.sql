a5aba532c5d641e7d216b31a18734905b20864ff
reshma981 <94059797+reshma981@users.noreply.github.com>
2021-12-26 20:31:31 +0530

    Create Register.sql

---
+++ Register.sql
@@ -0,0 +1,4 @@
+create database test;
+use test;
+create table register(id int primary key auto_increment,name varchar(255),email varchar(200),password varchar(255),sex varchar(255),country varchar(200));
+select * from register;
